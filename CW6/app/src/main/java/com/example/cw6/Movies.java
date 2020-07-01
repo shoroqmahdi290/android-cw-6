bb235d9a93b750888c389543dfbad1a5c5d2e974
shoroqmahdi290 <66693258+shoroqmahdi290@users.noreply.github.com>
2020-07-01 17:52:41 +0300

    CW 6 done

---
+++ CW6/app/src/main/java/com/example/cw6/Movies.java
@@ -0,0 +1,12 @@
+package com.example.cw6;
+
+import android.graphics.Movie;
+
+public class Movies {
+   private String MainActor;
+    private String genre;
+   private String title;
+   private Double MovieRate;
+   private int pgrate;
+
+}
